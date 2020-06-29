eb5e6c433149397e2c7e06fbf94609d1070833e2
alexandr <sashkaageev@gmail.com>
2020-06-30 00:45:36 +0300

    connect to MongoDB (in process...)

--- src/controllers/placesControllers.ts
+++ src/controllers/placesControllers.ts
@@ -4,41 +4,43 @@ import {validationResult} from 'express-validator'
 import {getCoordsForAddress} from "../utils/googleLocation";
 import Place, {IPlaceSchema} from '../models/Place';
 
-export let TEST_DATA = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  }
-];
-
 export const getPlaceById: RequestHandler = async (req, res, next) => {
   const placeId = req.params.pid;
-  const place = await TEST_DATA.find((el: any) => el.id === placeId);
+  let place;
+
+  try {
+    place = await Place.findById(placeId)
+  } catch (e) {
+    return next(new HttpError('Something went wrong, cold not find a place', 500))
+  }
+
   if (!place) return next(new HttpError('Could not find a place for the provided id', 404))
-  res.json({place})
+
+  res.json({place: place.toObject({getters: true})})
 }
 
 export const getPlaceByUserId: RequestHandler = async (req, res, next) => {
   const userId = req.params.uid;
-  const place = await TEST_DATA.find((el: any) => el.creator === userId);
-  if (!place) return next(new HttpError('Could not find a place for the provided userId', 404))
-  res.json({place})
+  let place;
+
+  try {
+    place = await Place.find({creator: userId})
+  } catch (e) {
+    return next(new HttpError('Something went wrong, could not find a place', 500))
+  }
+
+  if (!place || place.length === 0) {
+    return next(new HttpError('Could not find a place for the provided userId', 404))
+  }
+
+  res.json({place: place.map((el: IPlaceSchema) => el.toObject({getters: true}) )});
 }
 
 export const postCreatePlace: RequestHandler = async (req, res, next) => {
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) throw new HttpError('Invalid input passed, please check your date', 422)
 
   const {title, description, address, creator} = req.body;
-
   let coordinates;
 
   try {
@@ -59,7 +61,7 @@ export const postCreatePlace: RequestHandler = async (req, res, next) => {
   try {
     await createdPlace.save()
   } catch (error) {
-    const err =  new HttpError('Creating place failed, try again!', 500);
+    const err = new HttpError('Creating place failed, try again!', 500);
     return next(err)
   }
 
@@ -67,26 +69,41 @@ export const postCreatePlace: RequestHandler = async (req, res, next) => {
 }
 
 export const patchUpdatePlace: RequestHandler = async (req, res, next) => {
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) throw new HttpError('Invalid input passed, please check your date', 422)
 
   const placeId = req.params.pid;
   const {title, description} = req.body;
+  let place;
 
-  const updatedPlace: any = {...await TEST_DATA.find((el: any) => el.id === placeId)};
-  const placeIndex = await TEST_DATA.findIndex((el: any) => el.id === placeId);
+  try {
+    place = await Place.findById(placeId);
+  } catch (e) {
+    return next(new HttpError('Something went wrong, try again later', 401))
+  }
 
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  place!.title = title;
+  place!.description = description;
 
-  TEST_DATA[placeIndex] = updatedPlace;
+  try {
+    await place!.save()
+  } catch (e) {
+    return  next(new HttpError('Something went wrong, try again later', 422))
+  }
 
-  res.status(200).json({place: updatedPlace})
+  res.status(200).json({place: place!.toObject({getters: true})})
 
 }
 
 export const deletePlace: RequestHandler = async (req, res, next) => {
   const placeId = req.params.pid;
-  TEST_DATA = TEST_DATA.filter((el: any) => el.id !== placeId);
+
+  try {
+    await Place.findByIdAndDelete(placeId);
+  } catch (e) {
+    return next(new HttpError('Something went wrong, please try again later', 422))
+  }
+  
   res.status(200).json({message: 'Deleted place'})
 }
